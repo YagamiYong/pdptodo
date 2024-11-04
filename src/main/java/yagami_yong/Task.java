b13697d64c25ce9245da217431a6bea695efc6bd
zilola13 <bazarovazilola36@gmail.com>
2024-11-04 14:31:03 +0500

    updated version

--- src/main/java/yagami_yong/Task.java
+++ src/main/java/yagami_yong/Task.java
@@ -1,4 +1,28 @@
 package yagami_yong;
 
 public class Task {
+    private int id;
+    private String name;
+
+    public Task(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Task [id=" + id + ", name=" + name + "]";
+    }
 }

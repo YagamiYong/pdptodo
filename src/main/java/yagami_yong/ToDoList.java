df079ffd96d2f23808c6f76267057d50412314a7
zilola13 <bazarovazilola36@gmail.com>
2024-11-04 14:41:18 +0500

    the task of adding to the project has been completed

--- src/main/java/yagami_yong/ToDoList.java
+++ src/main/java/yagami_yong/ToDoList.java
@@ -14,33 +14,5 @@ public class ToDoList {
         System.out.println("Vazifa qo'shildi: " + task);
     }
 
-    public void deleteTask(int id) {
-        tasks.removeIf(task -> task.getId() == id);
-        System.out.println("ID " + id + " bilan vazifa o'chirildi.");
-    }
 
-    public void updateTask(int id, String newName) {
-        for (Task task : tasks) {
-            if (task.getId() == id) {
-                task.setName(newName);
-                System.out.println("Vazifa yangilandi: " + task);
-                return;
-            }
-        }
-        System.out.println("ID " + id + " bilan vazifa topilmadi.");
-    }
-
-    public List<Task> getTasks() {
-        return tasks;
-    }
-
-    public void printTasks() {
-        if (tasks.isEmpty()) {
-            System.out.println("Vazifalar ro'yxati bo'sh.");
-        } else {
-            for (Task task : tasks) {
-                System.out.println(task);
-            }
-        }
-    }
 }

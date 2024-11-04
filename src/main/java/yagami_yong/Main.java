a5c33870ddb4dd9ccb3f6205b962e26fbf336cf0
zilola13 <bazarovazilola36@gmail.com>
2024-11-04 14:56:48 +0500

    added main

--- src/main/java/yagami_yong/Main.java
+++ src/main/java/yagami_yong/Main.java
@@ -1,6 +1,22 @@
 package yagami_yong;
 public class Main {
     public static void main(String[] args) {
+        ToDoList todoList = new ToDoList();
 
+        todoList.addTask("Kitob o'qish");
+        todoList.addTask("Kod yozish");
+
+        System.out.println("\nBarcha vazifalar:");
+        todoList.printTasks();
+
+        todoList.updateTask(1, "Java kod yozish");
+
+        System.out.println("\nYangilangan vazifalar:");
+        todoList.printTasks();
+
+        todoList.deleteTask(2);
+
+        System.out.println("\nO'chirilganidan keyin vazifalar:");
+        todoList.printTasks();
     }
 }
